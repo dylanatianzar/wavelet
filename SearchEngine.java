1ba3b3acd3e97e8868c1a469beebe64ce6d8940a
dylanatianzar <dylanatianzar@gmail.com>
2023-01-19 13:33:49 -0800

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -3,8 +3,6 @@ import java.net.URI;
 import java.util.*;
 
 class Handler implements URLHandler {
-    // The one bit of state on the server: a number that will be manipulated by
-    // various requests.
     ArrayList<String> searchList = new ArrayList<String>();
     
     public String handleRequest(URI url) {
@@ -36,7 +34,7 @@ class Handler implements URLHandler {
     }
 }
 
-class NumberServer {
+class SearchEngine {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
             System.out.println("Missing port number! Try any number between 1024 to 49151");
