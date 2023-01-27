9d4b99d173fc18bb45ed83d10bed62da113627c3
dylanatianzar <69043855+dylanatianzar@users.noreply.github.com>
2023-01-26 23:10:31 -0800

    Update StringServer.java

--- StringServer.java
+++ StringServer.java
@@ -1,12 +1,32 @@
 import java.io.IOException;
 import java.net.URI;
+import java.util.*;
 
 class Handler implements URLHandler{
+    String wholeList = "";
+
     public String handleRequest(URI url){
+        if (url.getQuery() == null) {
+            return "Add a message to string with /add-message";
+        }
+        String[] parameters = url.getQuery().split("=");
+        if (url.getPath().contains("/add-message") && 
+                parameters[0].equals("s")){
+            wholeList += parameters[1] + "\n";
+            return wholeList;
+        }
         return "404 not found!";
     }
 }
 
 public class StringServer {
-    
+    public static void main(String[] args) throws IOException{
+        if (args.length == 0) {
+            System.out.println("Missing Port Number!");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+        Server.start(port, new Handler());
+    }
 }
